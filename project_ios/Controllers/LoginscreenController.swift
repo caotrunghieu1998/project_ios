f0835662b8f64566e481c82b20dc8169a3b569fd
caotrunghieu1998 <caotrunghieu.tdc2019@gmail.com>
2022-04-22 19:51:44 +0700

    Upload Code Base

---
+++ project_ios/Controllers/LoginscreenController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_ios
+//
+//  Created by Cao Trung Hieu on 22/04/2022.
+//
+
+import UIKit
+
+class LoginscreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
